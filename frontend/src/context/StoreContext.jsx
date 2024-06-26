1cbfc88f230ab13139aaa17ab15328b9f3fa96a4
Kavindu Thilan <kavinduthilan2000@outlook.com>
2024-06-26 09:57:54 +0530

    added function of addToCart, removeFromCart and getTotalCartAmount

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,17 +1,59 @@
 import { createContext } from 'react';
+import { useState } from 'react';
 import { food_list, menu_list } from '../assets/assets.js';
 
 export const StoreContext = createContext(null);
 
-const ContextProvider = ({children}) => {
+const ContextProvider = (props) => {
 
-    const contextValue = { food_list, menu_list };
+    const [amount, setAmount] = useState({});
     
-    console.log('ContextProvider food list :',food_list);
+    const addToCart = (itemId) => {
+        if(!amount[itemId]) {
+            setAmount((prev) => ({ ...prev, [itemId]: 1 }));
+        }
+        else {
+            setAmount((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        }
+    }
+
+    const removeFromCart = (itemId) => { 
+        setAmount((prev)=>({...prev,[itemId]: prev[itemId]-1}));
+    }
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in amount) {
+            if (amount[item] > 0) {
+                const itemInfo = food_list.find((product) => product._id === item);
+                if (itemInfo) {
+                    totalAmount += itemInfo.price * amount[item];
+                } else {
+                    console.log(`Item with _id ${item} not found in food_list.`);
+                    // Handle the case where itemInfo is undefined (optional)
+                }
+            }
+        }
+        return totalAmount;
+    };
+
+    
+    const contextValue = {
+        food_list,
+        menu_list,
+        amount,
+        addToCart,
+        removeFromCart,
+        getTotalCartAmount
+        
+    };
+    
+    
+    
 
     return (
         <StoreContext.Provider value={contextValue}>
-            {children}
+            {props.children}
         </StoreContext.Provider>
     )
 }
