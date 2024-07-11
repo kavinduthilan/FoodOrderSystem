8757c12f221e957f07bd5ffba9f480d2e0bf4419
Kavindu Thilan <kavinduthilan2000@outlook.com>
2024-07-12 00:16:42 +0530

    BrowserRoute

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ContextProvider from './context/StoreContext.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ContextProvider>
+  <BrowserRouter>
+    <ContextProvider>
       <App />
-  </ContextProvider>
+    </ContextProvider>
+  </BrowserRouter>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
