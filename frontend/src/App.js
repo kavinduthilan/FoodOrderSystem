a7b55227cbfafa358b498ad37ad7247e1422a435
Kavindu Thilan <kavinduthilan2000@outlook.com>
2024-07-12 00:17:06 +0530

    structred again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,20 @@
-import FoodDisplay from "./components/FoodDisplay";
+import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
-import { useState } from "react";
+import Home from "./pages/Home";
 
 
 
 function App() {
 
-  const [category, setCategory] = useState("All");
+  
   return (
     <>
       <div className="app">
         <Navbar />
-        <Header />
-        <Menu category={category} setCategory={setCategory}/>
-        <FoodDisplay category={category}/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
         <Footer />
     </>
